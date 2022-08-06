30b2144dddadcb36759185105fc3acb23313f8d7
Samuel M Vemagiri <vsamuelmoses@gmail.com>
2022-08-07 00:27:14 +0100

    UI cosmetic changes

--- Eldrow/Eldrow/Eldrow/Controls/Input.xaml.cs
+++ Eldrow/Eldrow/Eldrow/Controls/Input.xaml.cs
@@ -18,26 +18,5 @@ namespace Eldrow.Controls
             if (Text.ToUpper() != Text)
                 Text = Text.ToUpper();
         }
-
-        //public string InputTxt
-        //{
-        //    get { return (string)GetValue(InputTxtProperty); }
-        //    set { SetValue(InputTxtProperty, value); }
-        //}
-
-        //// Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
-        //public static readonly BindableProperty InputTxtProperty =
-        //    BindableProperty.Create("InputTxt", typeof(string), typeof(Input), "", defaultBindingMode: BindingMode.OneWay, null, propertyChanged:OnTextChanged);
-
-        //private static void OnTextChanged(BindableObject bindable, object oldValue, object newValue)
-        //{
-        //    var thisControl = (Input)bindable;
-        //    thisControl.UpdateText((string)newValue);
-        //}
-
-        //private void UpdateText(string newValue)
-        //{
-        //    Text = newValue?.ToUpper();
-        //}
     }
 }
