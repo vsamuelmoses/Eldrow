ac5294961eba5ac6b86686de36ab7bc90acfe24b
Samuel M Vemagiri <vsamuelmoses@gmail.com>
2022-08-16 11:21:08 +0100

    Added icons

--- Eldrow/Eldrow/Eldrow.Android/Resources - Copy/Resource.designer.cs
+++ Eldrow/Eldrow/Eldrow.Android/Resources - Copy/Resource.designer.cs
@@ -18796,13 +18796,7 @@ namespace Eldrow.Droid
 			public const int icon_round = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int launcher = 2131492866;
-			
-			// aapt resource value: 0x7F0C0003
-			public const int launcher_adaptive_back = 2131492867;
-			
-			// aapt resource value: 0x7F0C0004
-			public const int launcher_adaptive_fore = 2131492868;
+			public const int launcher_foreground = 2131492866;
 			
 			static Mipmap()
 			{
